eb462f0b85ef578823113e6cfdb918c649bea9f4
Tijesuconsult <ayoolablessing54@gmail.com>
2023-11-06 09:54:34 +0000

    Done

---
+++ script.js
@@ -0,0 +1,401 @@
+// const myform = document.getElementById ("form")
+const input = document.getElementById ("input")
+const grade = document.getElementById ("grade")
+
+form.addEventListener('submit', (event)=>{
+    event.preventDefault()
+    if(input.value > 100){
+        alert("math has to be between 0 and 100")
+    }
+    else if (input.value > 74){
+        grade.innerHTML = "A1"
+    }
+    else if (input.value > 69){
+        grade.innerHTML = "B2"
+    }
+    else if (input.value > 64){
+        grade.innerHTML = "B3"
+    }
+    else if (input.value > 59){
+        grade.innerHTML = "C4"
+    }
+    else if (input.value > 54){
+        grade.innerHTML = "C5"
+    }
+    else if (input.value > 49){
+        grade.innerHTML = "C6"
+    }
+    else if (input.value > 44){
+        grade.innerHTML = "D7"
+    }
+    else if (input.value > 39){
+        grade.innerHTML = "E8"
+    }
+    else if (input.value > 0){
+        grade.innerHTML = "F9"
+    }
+    else {
+        alert("Maths can't be empty")
+    }
+}
+)
+
+const grade2 = document.getElementById ("grade2")
+const input2 = document.getElementById ("input2")
+form.addEventListener('submit', (event)=>{
+    event.preventDefault()
+    if(input2.value > 100){
+        alert("English has to be between 0 and 100")
+    }
+    else if (input2.value > 74){
+        grade2.innerHTML = "A1"
+    }
+    else if (input2.value > 69){
+        grade2.innerHTML = "B2"
+    }
+    else if (input2.value > 64){
+        grade2.innerHTML = "B3"
+    }
+    else if (input2.value > 59){
+        grade2.innerHTML = "C4"
+    }
+    else if (input2.value > 54){
+        grade2.innerHTML = "C5"
+    }
+    else if (input2.value > 49){
+        grade2.innerHTML = "C6"
+    }
+    else if (input2.value > 44){
+        grade2.innerHTML = "D7"
+    }
+    else if (input2.value > 39){
+        grade2.innerHTML = "E8"
+    }
+    else if (input2.value > 0){
+        grade2.innerHTML = "F9"
+    }
+    else {
+        alert("English can't be empty")
+    }
+}
+)
+
+const grade3 = document.getElementById ("grade3")
+const input3 = document.getElementById ("input3")
+form.addEventListener('submit', (event)=>{
+    event.preventDefault()
+    if(input3.value > 100){
+        alert("Physics has to be between 0 and 100")
+    }
+    else if (input3.value > 74){
+        grade3.innerHTML = "A1"
+    }
+    else if (input3.value > 69){
+        grade3.innerHTML = "B2"
+    }
+    else if (input3.value > 64){
+        grade3.innerHTML = "B3"
+    }
+    else if (input3.value > 59){
+        grade3.innerHTML = "C4"
+    }
+    else if (input3.value > 54){
+        grade3.innerHTML = "C5"
+    }
+    else if (input3.value > 49){
+        grade3.innerHTML = "C6"
+    }
+    else if (input3.value > 44){
+        grade3.innerHTML = "D7"
+    }
+    else if (input3.value > 39){
+        grade3.innerHTML = "E8"
+    }
+    else if (input3.value > 0){
+        grade3.innerHTML = "F9"
+    }
+    else {
+        alert("Physics can't be empty")
+    }
+}
+)
+
+const grade4 = document.getElementById ("grade4")
+const input4 = document.getElementById ("input4")
+form.addEventListener('submit', (event)=>{
+    event.preventDefault()
+    if(input4.value > 100){
+        alert("Chemistry has to be between 0 and 100")
+    }
+    else if (input4.value > 74){
+        grade4.innerHTML = "A1"
+    }
+    else if (input4.value > 69){
+        grade4.innerHTML = "B2"
+    }
+    else if (input4.value > 64){
+        grade4.innerHTML = "B3"
+    }
+    else if (input4.value > 59){
+        grade4.innerHTML = "C4"
+    }
+    else if (input4.value > 54){
+        grade4.innerHTML = "C5"
+    }
+    else if (input4.value > 49){
+        grade4.innerHTML = "C6"
+    }
+    else if (input4.value > 44){
+        grade4.innerHTML = "D7"
+    }
+    else if (input4.value > 39){
+        grade4.innerHTML = "E8"
+    }
+    else if (input4.value > 0){
+        grade4.innerHTML = "F9"
+    }
+    else {
+        alert("Chemistry can't be empty")
+    }
+}
+)
+
+const grade5 = document.getElementById ("grade5")
+const input5 = document.getElementById ("input5")
+form.addEventListener('submit', (event)=>{
+    event.preventDefault()
+    if(input5.value > 100){
+        alert("Biology has to be between 0 and 100")
+    }
+    else if (input5.value > 74){
+        grade5.innerHTML = "A1"
+    }
+    else if (input5.value > 69){
+        grade5.innerHTML = "B2"
+    }
+    else if (input5.value > 64){
+        grade5.innerHTML = "B3"
+    }
+    else if (input5.value > 59){
+        grade5.innerHTML = "C4"
+    }
+    else if (input5.value > 54){
+        grade5.innerHTML = "C5"
+    }
+    else if (input5.value > 49){
+        grade5.innerHTML = "C6"
+    }
+    else if (input5.value > 44){
+        grade5.innerHTML = "D7"
+    }
+    else if (input5.value > 39){
+        grade5.innerHTML = "E8"
+    }
+    else if (input5.value > 0){
+        grade5.innerHTML = "F9"
+    }
+    else {
+        alert("Biology can't be empty")
+    }
+}
+)
+
+const grade6 = document.getElementById ("grade6")
+const input6 = document.getElementById ("input6")
+form.addEventListener('submit', (event)=>{
+    event.preventDefault()
+    if(input6.value >= 100){
+        alert("Economics has to be between 0 and 100")
+    }
+    else if (input6.value > 74){
+        grade6.innerHTML = "A1"
+    }
+    else if (input6.value > 69){
+        grade6.innerHTML = "B2"
+    }
+    else if (input6.value > 64){
+        grade6.innerHTML = "B3"
+    }
+    else if (input6.value > 59){
+        grade6.innerHTML = "C4"
+    }
+    else if (input6.value > 54){
+        grade6.innerHTML = "C5"
+    }
+    else if (input6.value > 49){
+        grade6.innerHTML = "C6"
+    }
+    else if (input6.value > 44){
+        grade6.innerHTML = "D7"
+    }
+    else if (input6.value > 39){
+        grade6.innerHTML = "E8"
+    }
+    else if (input6.value > 0){
+        grade6.innerHTML = "F9"
+    }
+    else {
+        alert("Economics can't be empty")
+    }
+}
+)
+const grade7 = document.getElementById ("grade7")
+const input7 = document.getElementById ("input7")
+form.addEventListener('submit', (event)=>{
+    event.preventDefault()
+    if(input7.value >= 100){
+        alert("Government has to be between 0 and 100")
+    }
+    else if (input7.value > 74){
+        grade7.innerHTML = "A1"
+    }
+    else if (input7.value > 69){
+        grade7.innerHTML = "B2"
+    }
+    else if (input7.value > 64){
+        grade7.innerHTML = "B3"
+    }
+    else if (input7.value > 59){
+        grade7.innerHTML = "C4"
+    }
+    else if (input7.value > 54){
+        grade7.innerHTML = "C5"
+    }
+    else if (input7.value > 49){
+        grade7.innerHTML = "C6"
+    }
+    else if (input7.value > 44){
+        grade7.innerHTML = "D7"
+    }
+    else if (input7.value > 39){
+        grade7.innerHTML = "E8"
+    }
+    else if (input7.value > 0){
+        grade7.innerHTML = "F9"
+    }
+    else {
+        alert("Government can't be empty")
+    }
+}
+)
+
+const grade8 = document.getElementById ("grade8")
+const input8 = document.getElementById ("input8")
+form.addEventListener('submit', (event)=>{
+    event.preventDefault()
+    if(input8.value >= 100){
+        alert("Civ-Edu has to be between 0 and 100")
+    }
+    else if (input8.value > 74){
+        grade8.innerHTML = "A1"
+    }
+    else if (input8.value > 69){
+        grade8.innerHTML = "B2"
+    }
+    else if (input8.value > 64){
+        grade8.innerHTML = "B3"
+    }
+    else if (input8.value > 59){
+        grade8.innerHTML = "C4"
+    }
+    else if (input8.value > 54){
+        grade8.innerHTML = "C5"
+    }
+    else if (input8.value > 49){
+        grade8.innerHTML = "C6"
+    }
+    else if (input8.value > 44){
+        grade8.innerHTML = "D7"
+    }
+    else if (input8.value > 39){
+        grade8.innerHTML = "E8"
+    }
+    else if (input8.value > 0){
+        grade8.innerHTML = "F9"
+    }
+    else {
+        alert("Civ-Edu can't be empty")
+    }
+}
+)
+
+const grade9 = document.getElementById ("grade9")
+const input9 = document.getElementById ("input9")
+form.addEventListener('submit', (event)=>{
+    event.preventDefault()
+    if(input9.value >= 100){
+        alert("Agric has to be between 0 and 100")
+    }
+    else if (input9.value > 74){
+        grade9.innerHTML = "A1"
+    }
+    else if (input9.value > 69){
+        grade9.innerHTML = "B2"
+    }
+    else if (input9.value > 64){
+        grade9.innerHTML = "B3"
+    }
+    else if (input9.value > 59){
+        grade9.innerHTML = "C4"
+    }
+    else if (input9.value > 54){
+        grade9.innerHTML = "C5"
+    }
+    else if (input9.value > 49){
+        grade9.innerHTML = "C6"
+    }
+    else if (input9.value > 44){
+        grade9.innerHTML = "D7"
+    }
+    else if (input9.value > 39){
+        grade9.innerHTML = "E8"
+    }
+    else if (input9.value > 0){
+        grade9.innerHTML = "F9"
+    }
+    else {
+        alert("Agric can't be empty")
+    }
+}
+)
+
+const grade10 = document.getElementById ("grade10")
+const input10 = document.getElementById ("input10")
+form.addEventListener('submit', (event)=>{
+    event.preventDefault()
+    if(input10.value >= 100){
+        alert("Yoruba has to be between 0 and 100")
+    }
+    else if (input10.value > 74){
+        grade10.innerHTML = "A1"
+    }
+    else if (input10.value > 69){
+        grade10.innerHTML = "B2"
+    }
+    else if (input10.value > 64){
+        grade10.innerHTML = "B3"
+    }
+    else if (input10.value > 59){
+        grade10.innerHTML = "C4"
+    }
+    else if (input10.value > 54){
+        grade10.innerHTML = "C5"
+    }
+    else if (input10.value > 49){
+        grade10.innerHTML = "C6"
+    }
+    else if (input10.value > 44){
+        grade10.innerHTML = "D7"
+    }
+    else if (input10.value > 39){
+        grade10.innerHTML = "E8"
+    }
+    else if (input10.value > 0){
+        grade10.innerHTML = "F9"
+    }
+    else {
+        alert("Yoruba can't be empty")
+    }
+}
+)
+
